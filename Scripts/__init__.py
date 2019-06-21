d5421756755bc11e473cb4118152a1bec38e2339
Zachary Labe <zlabe@uci.edu>
2019-06-21 09:36:19 -0700

    start repo

---
+++ Scripts/__init__.py
@@ -0,0 +1,10 @@
+'''
+The Python repository "StratoVari" contains the scripts, data,
+figures, and text for 2 AMIP-style simulations following the
+PAMIP protocol for 300 year periods. The model is SC-WACCM4.
+We are interested in the internal variability of the 
+stratospheric response to Arctic sea-ice loss. In particular, 
+is the response statistically robust in a very large ensemble 
+(n=300)?
+Author: Zachary Labe (zlabe@uci.edu)
+'''
---
+++ Scripts/read_MonthlyData.py
@@ -0,0 +1,118 @@
+"""
+Script reads in monthly data from PAMIP experiments using SC-WACCM4 
+for all 300 ensemble members! 
+ 
+Notes
+-----
+    Author : Zachary Labe
+    Date   : 21 June 2019
+    
+Usage
+-----
+    readExperiAll(directory,varid,timeperiod,level)
+"""
+
+def readExperiAll(varid,timeperiod,level):
+    """
+    Function reads monthly data from PAMIP simulations for 300 members
+
+    Parameters
+    ----------
+    varid : string
+        variable name to read
+    timeperiod: string
+        time of analysis (Future, Current, Past)
+    level : string
+        Height of variable (surface or profile)
+        
+
+    Returns
+    -------
+    lat : 1d numpy array
+        latitudes
+    lon : 1d numpy array
+        longitudes
+    var : 4d numpy array or 5d numpy array 
+        [year,month,lat,lon] or [year,month,level,lat,lon]
+
+    Usage
+    -----
+    lat,lon,lev,var = readExperiAll(varid,timeperiod,level)
+    """
+    print('\n>>> Using readExperiAll function!')
+    
+    ### Import modules
+    import numpy as np
+    from netCDF4 import Dataset
+    
+    ### Directory 1 for all ensemble members (remote server - seley)
+    directorydata1 = '/seley/zlabe/simu/'
+    
+    ###########################################################################
+    ###########################################################################
+    ###########################################################################
+    
+    ### Call files for directory 1 (1-100 members)
+    if timeperiod == 'Future':
+        experi = 'PAMIP_Fu'
+    elif timeperiod == 'Current':
+        experi = 'PAMIP_Cu'
+    elif timeperiod == 'Past':
+        experi = 'PAMIP_Pi'
+    else:
+        print(ValueError('Selected wrong time period (Future, Current, Past!')) 
+    totaldirectory = directorydata1 + experi + '/monthly/'
+    filename = totaldirectory + varid + '_1701-2000.nc'
+    
+    if varid == 'EGR' and level == 'surface':
+        filename = totaldirectory + varid + '_500_850.nc'
+
+    ### Read in Data
+    if level == 'surface': # 3d variables
+        data = Dataset(filename,'r')
+        lev = 'surface'
+        lat = data.variables['latitude'][:]
+        lon = data.variables['longitude'][:]
+        varq = data.variables['%s' % varid][:]
+        data.close()
+    elif level == 'profile': # 4d variables
+        data = Dataset(filename,'r')
+        lev = data.variables['level'][:]
+        lat = data.variables['latitude'][:]
+        lon = data.variables['longitude'][:]
+        varq = data.variables['%s' % varid][:]
+        data.close()
+    else:
+        print(ValueError('Selected wrong height - (surface or profile!)!'))    
+    print('Completed: Read data for *%s* : %s!' % (experi[:],varid))
+
+    ### Reshape to split years and months
+    months = 12
+    if level == 'surface': # 3d variables
+        var = np.reshape(varq,(int(varq.shape[0]/12),months,
+                              int(lat.shape[0]),int(lon.shape[0])))
+    elif level == 'profile': # 4d variables
+        var = np.reshape(varq,(int(varq.shape[0]/12),months,int(lev.shape[0]),
+                      int(lat.shape[0]),int(lon.shape[0])))
+    else:
+        print(ValueError('Selected wrong height - (surface or profile!)!')) 
+    print('Completed: Reshaped %s array!' % (varid))
+    
+    ### Convert units
+    if varid in ('TEMP','T2M'):
+        var = var - 273.15 # Kelvin to degrees Celsius 
+        print('Completed: Changed units (K to C)!')
+    elif varid == 'SWE':
+        var = var*1000. # Meters to Millimeters 
+        print('Completed: Changed units (m to mm)!')
+        
+    print('Completed: Read members 1-300!')
+
+    print('>>> Completed: Finished readExperiAll function!')
+    return lat,lon,lev,var
+
+#### Test function -- no need to use    
+varid = 'Z30'
+timeperiod = 'Future'
+level = 'surface'
+lat,lon,lev,var = readExperiAll(varid,timeperiod,level)
