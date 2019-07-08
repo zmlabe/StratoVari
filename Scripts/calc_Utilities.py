3b195e617f61598bf7abab03d084df66062c8686
Zachary Labe <zlabe@uci.edu>
2019-07-08 13:40:49 -0700

    FDR function

--- Scripts/calc_Utilities.py
+++ Scripts/calc_Utilities.py
@@ -10,12 +10,13 @@ Usage
 -----
     [1] calcDecJan(varx,vary,lat,lon,level,levsq)
     [2] calcDecJanFeb(varx,vary,lat,lon,level,levsq)
-    [3] calc_indttest(varx,vary)
-    [4] calc_weightedAve(var,lats)
-    [5] calc_spatialCorr(varx,vary,lats,lons,weight)
-    [6] calc_RMSE(varx,vary,lats,lons,weight)
-    [7] calc_spatialCorrHeight(varx,vary,lats,lons,weight)
-    [8] calc_spatialCorrHeightLev(varx,vary,lats,lons,weight,levelq)
+    [3] calc_FDR_ttest(varx,vary,alpha_f)
+    [4] calc_indttest(varx,vary)
+    [5] calc_weightedAve(var,lats)
+    [6] calc_spatialCorr(varx,vary,lats,lons,weight)
+    [7] calc_RMSE(varx,vary,lats,lons,weight)
+    [8] calc_spatialCorrHeight(varx,vary,lats,lons,weight)
+    [9] calc_spatialCorrHeightLev(varx,vary,lats,lons,weight,levelq)
 """
 
 def calcDecJan(varx,vary,lat,lon,level,levsq):
@@ -213,6 +214,67 @@ def calcDecJanFeb(varx,vary,lat,lon,level,levsq):
 
     print('*Completed: Finished calcDecJanFeb function!')
     return varx_djf
+
+###############################################################################
+###############################################################################
+###############################################################################
+    
+def calc_FDR_ttest(varx,vary,alpha_f):
+    """
+    Function first calculates statistical difference for 2 independent
+    sample t-test and then adjusts using a false discovery rate (FDR)
+    where alpha_o = alpha_FDR
+
+    Parameters
+    ----------
+    varx : 2d or 3d array
+    vary : 2d or 3d array
+    alpha_f : float (alpha_o = alpha_FDR)
+    
+    Returns
+    -------
+    pruns : 1d or 2d array of adjusted p values
+
+    Usage
+    -----
+    calc_FDR_ttest(varx,vary,alpha_f)
+    """
+    print('\n>>> Using calc_FDR_ttest function!')
+    
+    ### Import modules
+    import numpy as np
+    import scipy.stats as sts
+    import statsmodels.stats.multitest as fdr
+    
+    ### 2-independent sample t-test
+    stat,pvalue = sts.ttest_ind(varx,vary,nan_policy='omit')
+    
+    ### Ravel all 2d pvalues
+    if pvalue.ndim == 2:
+        pvalall = np.reshape(pvalue,(pvalue.shape[0]* pvalue.shape[1]))
+    else:
+        pvalall = pvalue
+    
+    ### Calculate false discovery rate
+    prunsq = np.empty((pvalall.shape))
+    score = np.empty((pvalall.shape))
+    prunsq.fill(np.nan)
+    score.fill(np.nan)
+    
+    ### Check for nans before correction!!
+    mask = np.isfinite(pvalall[:])
+    score[mask],prunsq[mask] = fdr.fdrcorrection(pvalall[mask],alpha=alpha_f,
+                                      method='indep')
+        
+    ### Reshape into correct dimensions
+    pruns = np.reshape(prunsq,(pvalue.shape))
+    
+    ### Mask variables by their adjusted p-values
+    pruns[np.where(pruns >= alpha_f)] = np.nan
+    pruns[np.where(pruns < alpha_f)] = 1.
+    
+    print('*Completed: Finished calc_FDR_ttest function!')
+    return pruns
 
 ###############################################################################
 ###############################################################################
