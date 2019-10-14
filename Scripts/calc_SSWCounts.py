91ce47b8fb85aaf5918fec5816432199aa29c76d
Zachary Labe <zlabe@uci.edu>
2019-10-14 08:45:59 -0700

    boreal winter SSW

--- Scripts/calc_SSWCounts.py
+++ Scripts/calc_SSWCounts.py
@@ -31,9 +31,26 @@ def calc_SSWCount(experi,time):
             ### Tally SSW totals
             count = ND + JFM
             freq = np.nansum(count)/10.
+            print(np.sum(ND))
+            print(np.sum(JFM))
+            print('Total SSW = %s' % np.sum(count))
             
             ### Save output
             np.savetxt(directorydata2 + 'NDJFM_SSW_%s' % experi,count)
+            
+        elif time == 'winter':
+            winter = np.empty((expn-1))
+            for i in range(expn-1):
+                filename = 'PAMIP-1.1-E3SM_%s/sswcount_%s_11-3_winter.txt' % (i+1,i+1)
+                winter[i] = np.genfromtxt(directorydata + filename,unpack=True)
+                
+            ### Tally SSW totals
+            count = winter
+            freq = np.nansum(count)/10.  
+            print('Total SSW = %s' % np.sum(count))
+            
+            ### Save output
+            np.savetxt(directorydata2 + 'winter_SSW_%s' % experi,count)
 ###############################################################################
 ###############################################################################
 ###############################################################################            
@@ -53,18 +70,36 @@ def calc_SSWCount(experi,time):
                 
             ### Tally SSW totals
             count = ND + JFM
-            freq = np.nansum(count)/10.
+            freq = np.nansum(count)/30.
+            print(np.sum(ND))
+            print(np.sum(JFM))
+            print('Total SSW = %s' % np.sum(count))
             
             ### Save output
             np.savetxt(directorydata2 + 'NDJFM_SSW_%s' % experi,count)
+            
+        elif time == 'winter':
+            winter = np.empty((expn-1))
+            for i in range(expn-1):
+                filename = 'PAMIP-1.1-QBO_%s/sswcount_%s_11-3_winter.txt' % (i+1,i+1)
+                winter[i] = np.genfromtxt(directorydata + filename,unpack=True)
+                
+            ### Tally SSW totals
+            count = winter
+            freq = np.nansum(count)/30.  
+            print('Total SSW = %s' % np.sum(count))
+            
+            ### Save output
+            np.savetxt(directorydata2 + 'winter_SSW_%s' % experi,count)
+            
 ###############################################################################
 ###############################################################################
 ###############################################################################
-    print('SSW Frequency - %s per decade!' % freq)        
+    print('SSW Frequency = %s per decade!' % freq)        
     return count,freq
             
 ### Test functions (do not use!)
-#count,freq = calc_SSWCount('E3SM_Cu','NDJFM')
-count,freq = calc_SSWCount('PAMIP_Cu','NDJFM')
+#count,freq = calc_SSWCount('E3SM_Cu','winter')
+count,freq = calc_SSWCount('PAMIP_Cu','winter')
             
         
